2fe17666da749a064c0bc4dd05388d0bec3b37e9
Subhradeep@23 <subhradeepchakraborty738@gmail.com>
2026-08-10 16:13:23 +0530

    Refactor containsDuplicate to use find instead of count

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>st;
-        int i=nums.size();
-        for(int i : nums){
-            if(st.count(i)){
+        unordered_set<int> st;
+
+        for (int x : nums) {
+            if (st.find(x) != st.end()) {
                 return true;
             }
-            st.insert(i);
 
+            st.insert(x);
         }
+
         return false;
-        
     }
 };
