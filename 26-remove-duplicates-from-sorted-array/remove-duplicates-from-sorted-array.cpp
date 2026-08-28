fb3279c9c744b75826f85f41ac5067dc55ee7dd2
Subhradeep@23 <subhradeepchakraborty738@gmail.com>
2026-08-28 12:07:42 +0530

    Fix duplicate removal logic in sorted array

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if(nums.empty()) return 0;
-        int k=1;
+        int k=0;
         for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[k-1]){
+            if(nums[i]!=nums[k]){
                 nums[k]=nums[i];
                 k++;
             }
